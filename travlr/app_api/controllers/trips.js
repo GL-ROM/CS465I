89457f59dc15ab77a6515b38a5a1461b55a9ab02
GL-ROM <gabriel.l.romero@gmail.com>
2023-02-27 03:17:05 -0500

    back end secure

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -114,29 +114,29 @@ const tripsUpdateTrip = async (req, res) => {
   )
 }
 
-// const getUser = (req, res, callback) => {
-//   if (req.payload && req.payload.email) {
-//     User
-//       .findOne({ email: req.payload.email })
-//       .exec((err, user) => {
-//         if (!user) {
-//           return res
-//               .status(404)
-//               .json({ "message": "User not found" });
-//         } else if (err) {
-//           console.log(err);
-//           return res
-//               .status(404)
-//               .json(err); 
-//         }
-//         callback(req,res, user.name);
-//       });
-//   } else {
-//     return res
-//         .status(404)
-//         .json({"message": "User not found" });
-//   }
-// };
+const getUser = (req, res, callback) => {
+  if (req.payload && req.payload.email) {
+    User
+      .findOne({ email: req.payload.email })
+      .exec((err, user) => {
+        if (!user) {
+          return res
+              .status(404)
+              .json({ "message": "User not found" });
+        } else if (err) {
+          console.log(err);
+          return res
+              .status(404)
+              .json(err); 
+        }
+        callback(req,res, user.name);
+      });
+  } else {
+    return res
+        .status(404)
+        .json({"message": "User not found" });
+  }
+};
 
 module.exports = {
   tripsList,
